c467f7ad8d9aea2a083383d04f788d04c72debb3
João Vitor <joaovitorpd@gmail.com>
2022-09-13 10:44:33 -0300

    initial commit with API and Local Storage implemented

---
+++ lib/src/todos_api.dart
@@ -0,0 +1,21 @@
+import 'package:todos_api/todos_api.dart';
+
+/// {@template todos_api}
+/// The interface and models for an API providing access to todos.
+/// {@endtemplate}
+abstract class TodosApi {
+  /// {@macro todos_api}
+  const TodosApi();
+
+  Stream<List<Todo>> getTodos();
+
+  Future<void> saveTodo(Todo todo);
+
+  Future<void> deleteTodo(String id);
+
+  Future<int> clearCompleted();
+
+  Future<int> completeAll({required bool isCompleted});
+}
+
+class TodoNotFoundException implements Exception {}
