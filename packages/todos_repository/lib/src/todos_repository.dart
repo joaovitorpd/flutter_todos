acb5649cc141b0cb803b4a5f6e875773ff6b4768
João Vitor <joaovitorpd@gmail.com>
2022-09-13 11:04:04 -0300

    Path rebuild and repository implemented

---
+++ packages/todos_repository/lib/src/todos_repository.dart
@@ -0,0 +1,24 @@
+import 'package:todos_api/todos_api.dart';
+
+/// {@template todos_repository}
+/// A repository that handles todo related requests.
+/// {@endtemplate}
+class TodosRepository {
+  /// {@macro todos_repository}
+  const TodosRepository({
+    required TodosApi todosApi,
+  }) : _todosApi = todosApi;
+
+  final TodosApi _todosApi;
+
+  Stream<List<Todo>> getTodos() => _todosApi.getTodos();
+
+  Future<void> saveTodo(Todo todo) => _todosApi.saveTodo(todo);
+
+  Future<void> deleteTodo(String id) => _todosApi.deleteTodo(id);
+
+  Future<int> clearCompleted() => _todosApi.clearCompleted();
+
+  Future<int> completeAll({required bool isCompleted}) =>
+      _todosApi.completeAll(isCompleted: isCompleted);
+}
